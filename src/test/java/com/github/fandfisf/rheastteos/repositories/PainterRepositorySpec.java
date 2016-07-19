636d8e2856dc68dbda47868645d4320f72d344d6
Prashant Khanwale <pskhanwale@gmail.com>
2016-07-18 21:05:57 -0700

    Add a test for Painter Repository
    We add a test for the Painter Repository

---
+++ src/test/java/com/github/fandfisf/rheastteos/repositories/PainterRepositorySpec.java
@@ -0,0 +1,45 @@
+package com.github.fandfisf.rheastteos.repositories;
+
+import com.github.fandfisf.rheastteos.RheastteosApplication;
+import com.github.fandfisf.rheastteos.model.Painter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Prashant S Khanwale @ Suveda LLC  on 7/16/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RheastteosApplication.class)
+@WebAppConfiguration
+public class PainterRepositorySpec {
+    @Autowired  private PainterRepository target = null;
+    @Test
+    public void yellowCanary(){
+        assertNotNull(target);
+    }
+    @Test
+    public void pullOneById(){
+        final Painter vvg = target.findOne(1L);
+        assertNotNull(vvg);
+        assertEquals(vvg.getPseudonym(),"The Little Painter Fellow");
+    }
+    @Test
+    public void pullByPseudonym(){
+        final Painter sp = target.findByPseudonym("Pre-historic San People").get(0);
+        assertNotNull(sp);
+        assertEquals(sp.getFirstName(),"San");
+    }
+    @Test
+    public void pullByLastName(){
+        final Painter sp = target.findByLastName("van Gogh").get(0);
+        assertNotNull(sp);
+        assertEquals(sp.getFirstName(),"Vincent");
+    }
+}
